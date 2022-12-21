66e1c83775c7eb318bd63d5d33c112980fab61c8
HP <nikamsushant242@gmail.com>
2022-12-21 10:43:55 +0530

    hello friends

---
+++ demo_ec_SDM/src/demo_ec_SDM/HelloDemo.java
@@ -0,0 +1,10 @@
+package demo_ec_SDM;
+
+public class HelloDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello friends ........lelo");
+	}
+
+}
---
+++ demo_ec_SDM/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author HP
+ *
+ */
+module demo_ec_SDM {
+}
